e2d79494650284b9e690798e36ec09acca63f134
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-06-11 15:06:37 +0200

    one to many push

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'image', 'content', 'slug'];
+    protected $fillable = ['title', 'image', 'content', 'slug', 'user_id', 'category_id'];
+    //protected $guarded = [];
 
     public static function generateSlug($title)
     {
@@ -20,5 +22,14 @@ class Post extends Model
             $count++;
         }
         return $slug;
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
